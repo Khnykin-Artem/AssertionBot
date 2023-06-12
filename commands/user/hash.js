028c125bd0a0bc2682eca6b7155a12ef0588fb17
Khnykin-Artem <k98030118@gmail.com>
2023-06-12 20:36:45 +0300

    d

--- commands/user/hash.js
+++ commands/user/hash.js
@@ -1,4 +1,23 @@
+const hashing = require('../../lib/hashing');
+const { validationErrorText } = require('../../texts');
+
 module.exports = {
   name: 'hash',
-  handler: async (ctx) => {},
+  handler: async (ctx) => {
+    try {
+      const [string, length] = ctx.message.text.split(' ').slice(1);
+
+      if (!string) {
+        return ctx.reply(validationErrorText('string'));
+      }
+      if (!length) {
+        return ctx.reply(validationErrorText('length'));
+      }
+
+      const hash = await hashing(string, length);
+      return ctx.reply(hash);
+    } catch (e) {
+      return console.log(e);
+    }
+  },
 };
---
+++ commands/user/verify.js
@@ -0,0 +1,23 @@
+const { validationErrorText } = require('../../texts');
+const compare = require('../../lib/compare');
+
+module.exports = {
+  name: 'verify',
+  handler: async (ctx) => {
+    try {
+      const [testString, hash] = ctx.message.text.split(' ').slice(1);
+
+      if (!testString) {
+        return ctx.reply(validationErrorText('testString'));
+      }
+      if (!hash) {
+        return ctx.reply(validationErrorText('hash'));
+      }
+
+      const comparisonResult = await compare(testString, hash);
+      return ctx.reply(String(comparisonResult));
+    } catch (e) {
+      return console.log(e);
+    }
+  },
+};
